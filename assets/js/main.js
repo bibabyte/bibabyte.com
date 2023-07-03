1c32e6aceb8108f2b9de331013d2cee868b48b00
Biba <bhagavati.braun@gmail.com>
2023-07-03 11:50:42 +0200

    JS link experiment
    JS scripts aren't working on pages other than main (click off closes menu)
    An alert test was made and it runs on all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+// alert("yo");
 
 
 // TO CLOSE THE MENU AFTER CLICING A SAME-PAGE-ITEM
@@ -22,6 +23,8 @@ var closeIcon = document.getElementById("menuTog");
       }
 
 // TO CLOSE THE MENU AFTER CLICKING OUTSIDE THE MENU
+
+
 $('#nav-menu').click(function(){
 
 	$('#menuContent').show(function(){
