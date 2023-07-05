84f515d704b42d23ef7234279273f74f486f6c28
Biba <bhagavati.braun@gmail.com>
2023-07-05 10:47:08 +0200

    Dialog Button
    More options: able to include links
    Hours is connected.

--- portfolio/coiffeur/js/main.js
+++ portfolio/coiffeur/js/main.js
@@ -22,5 +22,46 @@ var closeIcon = document.getElementById("menuTog");
 // POPUP on menu click
 
 function hours() {
+    closeIcon.checked = false;
     alert("du mardi au samedi 10h30-19h30");
+   
 }
+
+// function findUs() {
+//     closeIcon.checked = false;
+//     alert(<a href='https://www.google.com/maps/place/12+Rue+Marie+et+Louise,+75010+Paris/@48.8720847,2.3643842,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e0a3f4f73e1:0x57d53d5992b0e21c!8m2!3d48.8720847!4d2.3665782!16s%2Fg%2F11c3q3hj3x'>12 rue Marie et Louise <br>
+//     75010  Paris</a>);
+// }
+
+
+//ALT POPUP
+
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("hours");
+const outputBox = document.querySelector("output");
+// const selectEl = favDialog.querySelector("select");
+// const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+// "Show the dialog" button opens the <dialog> modally
+showButton.addEventListener("click", () => {
+  favDialog.showModal();
+});
+
+// "Favorite animal" input sets the value of the submit button
+// selectEl.addEventListener("change", (e) => {
+//   confirmBtn.value = selectEl.value;
+// });
+
+// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+favDialog.addEventListener("close", (e) => {
+  outputBox.value =
+    favDialog.returnValue === "default"
+      ? "No return value."
+      : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+});
+
+// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+// confirmBtn.addEventListener("click", (event) => {
+//   event.preventDefault(); // We don't want to submit this fake form
+//   favDialog.close(selectEl.value); // Have to send the select box value here.
+// });
