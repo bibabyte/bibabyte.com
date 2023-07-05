ee2e613b58c091f086fbbca8932ec453e2ab407a
Biba <bhagavati.braun@gmail.com>
2023-07-05 11:53:55 +0200

    Menu closes on Dialogue close
    dialogue boxes that open from menu (home and location)
    open on click and close on click -- the menu also closes on the close

--- portfolio/coiffeur/js/main.js
+++ portfolio/coiffeur/js/main.js
@@ -21,20 +21,6 @@ var closeIcon = document.getElementById("menuTog");
 
 // POPUP on menu click
 
-// function hours() {
-//     closeIcon.checked = false;
-//     alert("du mardi au samedi 10h30-19h30");
-   
-// }
-
-// function findUs() {
-//     closeIcon.checked = false;
-//     alert(<a href='https://www.google.com/maps/place/12+Rue+Marie+et+Louise,+75010+Paris/@48.8720847,2.3643842,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e0a3f4f73e1:0x57d53d5992b0e21c!8m2!3d48.8720847!4d2.3665782!16s%2Fg%2F11c3q3hj3x'>12 rue Marie et Louise <br>
-//     75010  Paris</a>);
-// }
-
-
-//ALT POPUP
 
 const showButton = document.getElementById("showDialog");
 const showButton2 = document.getElementById("showDialog2");
@@ -46,16 +32,17 @@ const favDialog2 = document.getElementById("location");
 
 // "Show the dialog" button opens the <dialog> modally
 showButton.addEventListener("click", () => {
-  favDialog.showModal();
+    favDialog.showModal();
 });
+
+favDialog.addEventListener("click", () => {
+    closeIcon.checked = false;
+})
 
 showButton2.addEventListener("click", () => {
     favDialog2.showModal();
 });
 
-
-// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
-// confirmBtn.addEventListener("click", (event) => {
-//   event.preventDefault(); // We don't want to submit this fake form
-//   favDialog.close(selectEl.value); // Have to send the select box value here.
-// });
+favDialog2.addEventListener("click", () => {
+    closeIcon.checked = false;
+})
