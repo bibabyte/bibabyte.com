9d3e1936c3a03a010f94e58892f54e3e53691dcc
Biba <bhagavati.braun@gmail.com>
2023-07-10 10:30:19 +0200

    Menu and Dialog Box
    design factors, added map

--- portfolio/coiffeur/js/main.js
+++ portfolio/coiffeur/js/main.js
@@ -3,63 +3,65 @@
 
 
 // TO CLOSE THE MENU AFTER CLICKING A SAME-PAGE-ITEM
-var menu = document.getElementById('nav-menu');
-var menuContent = document.getElementById('menuContent');
-var backgroundOverlay = document.getElementById('backgroundOverlay');
+// WORKS BUT only works on popup, when it closes the menu breaks
+// var menu = document.getElementById('nav-menu');
+// var menuContent = document.getElementById('menuContent');
+// var backgroundOverlay = document.getElementById('backgroundOverlay');
 
-var closeIcon = document.getElementById("menuTog");
+// var closeIcon = document.getElementById("menuTog");
 
 
-    menu.addEventListener('click', handleMenuClick);
+//     menu.addEventListener('click', handleMenuClick);
 
-    menu.addEventListener('touch', handleMenuClick);
+//     menu.addEventListener('touch', handleMenuClick);
 
-    function handleMenuClick(event) {
-        if (event.target instanceof HTMLAnchorElement){
-            closeIcon.checked = false;
-        }
-    }
+//     function handleMenuClick(event) {
+//         if (event.target instanceof HTMLAnchorElement){
+//             closeIcon.checked = false;
+//         }
+//     }
 
       // TO CLOSE THE MENU AFTER CLICKING OUTSIDE THE MENU
 
-    //   document.
-      
-    //   onclick = function(e){
-    //     if(e.target.id !== 'nav-menu'){
-    //       //element clicked wasn't the div; hide the div
-    //       menuContent.style.display = 'none';
-    //     }
-    //   };
+// document.addEventListener('click', closeIf);
+
+// function closeIf(locOfClick) {
+//     if (locOfClick.target instanceof HTMLAnchorElement) {
+//         closeIcon.checked = true;
+//     }
+// }
+    
+
+
+// If the menu box is unchecked it should still work.
 
 
 
 
+// POPUP dialog on menu click
 
 
-// POPUP on menu click
+const showHours = document.getElementById("show-hours");
+const showLocation = document.getElementById("show-location");
+const hours = document.getElementById("hours");
+const loc = document.getElementById("location");
 
-
-const showButton = document.getElementById("showDialog");
-const showButton2 = document.getElementById("showDialog2");
-const favDialog = document.getElementById("hours");
-const favDialog2 = document.getElementById("location");
-
-showButton.addEventListener("click", () => {
-    favDialog.showModal();
+showHours.addEventListener("click", () => {
+    hours.showModal();
 });
 
-showButton2.addEventListener("click", showDialog);
-// showButton2.addEventListener("click", () => {
-//     favDialog2.showModal();
-// });
+showLocation.addEventListener("click", showDialog);
+showLocation.addEventListener("click", () => {
+    loc.showModal();
+});
 
 
 
-favDialog.addEventListener("click", menuClose);
-favDialog.addEventListener("touch", menuClose);
+hours.addEventListener("click", menuClose);
+hours.addEventListener("touch", menuClose);
 
-favDialog2.addEventListener("click", menuClose);
-favDialog2.addEventListener("touch", menuClose);
+loc.addEventListener("click", menuClose);
+loc.addEventListener("touch", menuClose);
 
 function menuClose() {
     closeIcon.checked = false;
