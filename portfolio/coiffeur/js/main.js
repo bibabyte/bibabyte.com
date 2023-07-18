0e615d30c84ee6a1b0a55845b98db5a6f992c116
Biba <bhagavati.braun@gmail.com>
2023-07-18 17:50:37 +0200

    Menu on Coiffeur basic works
    Click on menu closes menu
    Click off of menu doesnt' close -- but MVP, it's OK for now

--- portfolio/coiffeur/js/main.js
+++ portfolio/coiffeur/js/main.js
@@ -1,48 +1,101 @@
 
 // alert('why');
 
-
-// TO CLOSE THE MENU AFTER CLICING A SAME-PAGE-ITEM
+// TO CLOSE THE MENU AFTER CLICKING A SAME-PAGE-ITEM
 var menu = document.getElementById('nav-menu');
+var menuContent = document.getElementById('menuContent');
+var backgroundOverlay = document.getElementById('backgroundOverlay');
+var cl = document.getElementById("mainContent");
 
 var closeIcon = document.getElementById("menuTog");
 
+menu.addEventListener('click', handleMenuClick);
 
-    menu.addEventListener('click', handleMenuClick);
+menu.addEventListener('touch', handleMenuClick);
 
-    menu.addEventListener('touch', handleMenuClick);
+function handleMenuClick(event) {
+    if (event.target instanceof HTMLAnchorElement){
+        closeIcon.checked = false;
+    }
+}
 
-    function handleMenuClick(event) {
-        if (event.target instanceof HTMLAnchorElement){
-            closeIcon.checked = false;
-        }
-      }
+// TO CLOSE THE MENU AFTER CLICKING OUTSIDE THE MENU
+
+cl.addEventListener("click", hide_menu);
+
+function hide_menu(){
+
+    // var menu = document.getElementById('dropdown_menu');
+    if(menu.style.display == 'none'){
+        menu.style.display = 'block';
+    }
+}
+
+
+
+// document.addEventListener('click', closeIf());
+
+//  function closeIf(locOfClick) {
+//      if (locOfClick.target instanceof HTMLAnchorElement) {
+//          closeIcon.checked = true;
+//      }
+
+//  };
+
+    // onclick = function(e){
+    //     if(e.target.id !== 'nav-menu'){		
+    //       //element clicked wasn't the div; hide the div		
+    //       menuContent.style.display = 'none';		
+    //     }		
+    //   };
+
+
+
+// TO CLOSE THE MENU AFTER CLICING A SAME-PAGE-ITEM
+// var menu = document.getElementById('nav-menu');
+
+// var closeIcon = document.getElementById("menuTog");
+
+
+//     menu.addEventListener('click', handleMenuClick);
+
+//     menu.addEventListener('touch', handleMenuClick);
+
+//     function handleMenuClick(event) {
+//         if (event.target instanceof HTMLAnchorElement){
+//             closeIcon.checked = false;
+//         }
+//       }
+
+
+
+
 
 
 // POPUP on menu click
 
 
-const showButton = document.getElementById("showDialog");
-const showButton2 = document.getElementById("showDialog2");
-const favDialog = document.getElementById("hours");
-const favDialog2 = document.getElementById("location");
-// const outputBox = document.querySelector("output");
-// const selectEl = favDialog.querySelector("select");
-// const confirmBtn = favDialog.querySelector("#confirmBtn");
+// const showButton = document.getElementById("showDialog");
+// const showButton2 = document.getElementById("showDialog2");
+// const favDialog = document.getElementById("hours");
+// const favDialog2 = document.getElementById("location");
+// // const outputBox = document.querySelector("output");
+// // const selectEl = favDialog.querySelector("select");
+// // const confirmBtn = favDialog.querySelector("#confirmBtn");
 
-// "Show the dialog" button opens the <dialog> modally
-showButton.addEventListener("click", () => {
-    favDialog.showModal();
-});
+// // "Show the dialog" button opens the <dialog> modally
+// showButton.addEventListener("click", () => {
+//     favDialog.showModal();
+// });
 
-favDialog.addEventListener("click", () => {
-    closeIcon.checked = false;
-})
+// favDialog.addEventListener("click", () => {
+//     closeIcon.checked = false;
+// })
 
-showButton2.addEventListener("click", () => {
-    favDialog2.showModal();
-});
+// showButton2.addEventListener("click", () => {
+//     favDialog2.showModal();
+// });
 
-favDialog2.addEventListener("click", () => {
-    closeIcon.checked = false;
-})
+// favDialog2.addEventListener("click", () => {
+//     closeIcon.checked = false;
+// })
